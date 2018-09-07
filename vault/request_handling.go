e63350c64f0a238a32f13a43b373200d9083ba1b
m.conraux <m.conraux@criteo.com>
2018-09-07 14:53:56 +0200

    initial commit for prometheus and sys/metrics support

--- vault/request_handling.go
+++ vault/request_handling.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/armon/go-metrics"
+	metrics "github.com/armon/go-metrics"
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/go-multierror"
 	sockaddr "github.com/hashicorp/go-sockaddr"
@@ -36,10 +36,12 @@ var (
 // HandlerProperties is used to seed configuration into a vaulthttp.Handler.
 // It's in this package to avoid a circular dependency
 type HandlerProperties struct {
-	Core                  *Core
-	MaxRequestSize        int64
-	MaxRequestDuration    time.Duration
-	DisablePrintableCheck bool
+	Core                         *Core
+	MaxRequestSize               int64
+	MaxRequestDuration           time.Duration
+	DisablePrintableCheck        bool
+	TelemetryMemSink             *metrics.InmemSink
+	TelemetryPrometheusRetention time.Duration
 }
 
 // fetchEntityAndDerivedPolicies returns the entity object for the given entity
